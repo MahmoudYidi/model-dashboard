4ff7cd64d96ca5510edab06423af79ccc9f020fb
MahmoudYidi <dopemubiks@gmail.com>
2025-07-02 12:41:31 +0100

    mobile

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY') or 'dev-secret-key'
     API_SECRET_KEY = os.environ.get('API_SECRET_KEY')
+    #API_SECRET_KEY='47c153110cf7eb8fe2c16283b89f6927'
     DEBUG = os.getenv('DEBUG') == 'True'
